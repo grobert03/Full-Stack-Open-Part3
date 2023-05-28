44571cbfbcee08c04da943a4ac6e1f8e26188d93
grobert03 <gainarobert47@gmail.com>
2023-05-28 16:17:06 +0200

    Finished exercise 3.9

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
+app.use(cors());
 app.use(express.json());
 
 morgan.token('data', (req) => {
